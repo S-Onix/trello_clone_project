4583ddfb6439574961214eabb805f1d66e7ea997
S-Onix <msyoo101@hansung.ac.kr>
2020-11-25 16:58:47 +0900

    create board vue

--- vue_trello_clone/src/components/Navbar.vue
+++ vue_trello_clone/src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <div>
     <router-link to="/">Home</router-link>
     <router-link to="/login">Login</router-link>
+    
   </div>
 </template>
 
--- vue_trello_clone/src/router/index.js
+++ vue_trello_clone/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Home from '../components/Home'
 import Login from '../components/Login'
 import NotFound from '../components/NotFound'
+import Board from '../components/Board'
 import VueRouter from 'vue-router'
 
 // Vue Router 사용하기
@@ -12,6 +13,7 @@ const router = new VueRouter({
   routes : [
     {path : '/' , component : Home},
     {path : '/login', component : Login},
+    {path : '/b/:bid', component : Board}, // bid라는 변수가 들어간다.
     {path: '*', component : NotFound} //위의 규칙을 따르지 않을시 가장 아래 데이터와 메핑된다.
   ]
 })
