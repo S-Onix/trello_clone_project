50e6257ee52ba16879813ada8e042903021d0d75
S-Onix <msyoo101@hansung.ac.kr>
2020-11-25 16:07:21 +0900

    vue-router adapt

--- vue_trello_clone/src/main.js
+++ vue_trello_clone/src/main.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+// Vue Router 사용하기
+Vue.use(VueRouter)
 
 const Login = {
   template : `<div>Login Page</div>`
 }
-
-const routes = {
-  '/' : App,
-  '/login' : Login
+const NotFound = {
+  template : `<div>Page not Found</div>`
 }
+
+const router = new VueRouter({
+  mode : 'history',
+  routes : [
+    {path : '/' , component : App},
+    {path : '/login', component : Login},
+    {path: '*', component : NotFound} //위의 규칙을 따르지 않을시 가장 아래 데이터와 메핑된다.
+  ]
+})
 
 new Vue({
   el: '#app',
-  computed : {
-    VueComponent() {
-      return routes[window.location.pathname] ||
-      { template : `<div>Page Not Found</div>`}
-    }
-  },
-
-  render(h) {
-    return h(this.VueComponent)
-  }
-
+  router,
+  render: h=>h({template: '<router-view></router-view>'})
 
 })
