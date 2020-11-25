de0157cd5208cdcdd236797eeb490fa09dfa2549
S-Onix <msyoo101@hansung.ac.kr>
2020-11-25 16:11:42 +0900

    separate vue-router form main.js and mk folder

--- vue_trello_clone/src/main.js
+++ vue_trello_clone/src/main.js
@@ -1,25 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-
-// Vue Router 사용하기
-Vue.use(VueRouter)
-
-const Login = {
-  template : `<div>Login Page</div>`
-}
-const NotFound = {
-  template : `<div>Page not Found</div>`
-}
-
-const router = new VueRouter({
-  mode : 'history',
-  routes : [
-    {path : '/' , component : App},
-    {path : '/login', component : Login},
-    {path: '*', component : NotFound} //위의 규칙을 따르지 않을시 가장 아래 데이터와 메핑된다.
-  ]
-})
+import router from './router'
 
 new Vue({
   el: '#app',
---
+++ vue_trello_clone/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from '../App.vue'
+import VueRouter from 'vue-router'
+
+// Vue Router 사용하기
+Vue.use(VueRouter)
+
+const Login = {
+  template : `<div>Login Page</div>`
+}
+const NotFound = {
+  template : `<div>Page not Found</div>`
+}
+
+const router = new VueRouter({
+  mode : 'history',
+  routes : [
+    {path : '/' , component : App},
+    {path : '/login', component : Login},
+    {path: '*', component : NotFound} //위의 규칙을 따르지 않을시 가장 아래 데이터와 메핑된다.
+  ]
+})
+
+// 모듈로 사용할수 있게 export해준다.
+export default router
